f60fcd22d75ff7963f8b5ef9f3193cf52d302c93
HaeJun Seo <kan02134@gmail.com>
2020-06-03 00:06:38 +0900

    Feat: Add url check logic

--- browser/js/index.js
+++ browser/js/index.js
@@ -1,34 +1,50 @@
 window.addEventListener('load', () => {
-  // declare link click evt for chrome extension app
-  document.querySelectorAll('a').forEach((el) => {
-    el.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      chrome.tabs.create({ url: el.getAttribute('href') });
+  chrome.tabs.query({
+    active: true,
+    windowId: chrome.windows.WINDOW_ID_CURRENT,
+  }, ([{ url }]) => {
+    // check available url
+    const isAvailableUrl = [
+      /lms\.sch\.ac\.kr/,
+      /commons\.sch\.ac\.kr/,
+    ].reduce((res, r) => res = (url.match(r) !== null) || res, false);
+
+    if (!isAvailableUrl) {
+      document.body.innerHTML = url;
+      return false;
+    }
+
+    // declare link click evt for chrome extension app
+    document.querySelectorAll('a').forEach((el) => {
+      el.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        chrome.tabs.create({ url: el.getAttribute('href') });
+      });
     });
-  });
 
-  // init btns
-  ['session', 'playback', 'course'].forEach((name) => {
-    // get el
-    const target = document.querySelector(`span.btn[${name}]`);
+    // init btns
+    ['session', 'playback', 'course'].forEach((name) => {
+      // get el
+      const target = document.querySelector(`span.btn[${name}]`);
 
-    // get setting values
-    chrome.storage.sync.get([name], (st) => {
-      target.classList.add(!!st[name] ? 'on' : 'off');
+      // get setting values
+      chrome.storage.sync.get([name], (st) => {
+        target.classList.add(!!st[name] ? 'on' : 'off');
 
-      target.addEventListener('click', () => {
-        // update setting value
-        chrome.storage.sync.set({ [name]: !(!!st[name]) });
+        target.addEventListener('click', () => {
+          // update setting value
+          chrome.storage.sync.set({ [name]: !(!!st[name]) });
 
-        // reload page
-        chrome.tabs.getSelected(null, ({ id }) => {
-          chrome.tabs.executeScript(id, {
-            code: 'window.location.reload();',
+          // reload page
+          chrome.tabs.getSelected(null, ({ id }) => {
+            chrome.tabs.executeScript(id, {
+              code: 'window.location.reload();',
+            });
           });
-        });
 
-        // reload extension
-        window.location.reload();
+          // reload extension
+          window.location.reload();
+        });
       });
     });
   });
